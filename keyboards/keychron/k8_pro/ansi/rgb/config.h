303eef4eb11a997f390d3c7d709d31aa227c666c
Charles Stein <charles.mst@gmail.com>
2022-08-06 17:22:29 -0300

    better config for bottom row control

--- keyboards/keychron/k8_pro/ansi/rgb/config.h
+++ keyboards/keychron/k8_pro/ansi/rgb/config.h
@@ -74,5 +74,14 @@
 
 /* Set LED driver current */
 #define CKLED2001_CURRENT_TUNE   {0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38, 0x38}
+/* Custom config for karabiner keymap */
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
 
 #endif
