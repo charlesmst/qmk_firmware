8d01d8b245ae97cc526f36dcad08363c78104100
Charles Stein <charles.mst@gmail.com>
2022-08-26 22:40:34 -0300

    add ctrl in right command

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/karabiner/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/karabiner/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,    KC_END,   KC_PGDN,
      LT(LAYER_CAPS,KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     LT(LAYER_CAPS,KC_SCLN),  KC_QUOT,            KC_ENT,             
      KC_LSFT,            LCTL_T(KC_Z),     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RCTL_T(KC_SLSH),            KC_RSFT,            KC_UP,    
-     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN, MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_LCTL, KC_ROPTN, MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_ansi_87( 
      KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LNPD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,             KC_PSCR,  KC_TRNS,  RGB_TOG,
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_CAPS] = LAYOUT_ansi_87( 
      CAPSWRD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  LALT(LSFT(KC_1)),  LALT(LSFT(KC_2)),  LALT(LSFT(KC_3)),  LALT(LSFT(KC_4)),  LALT(LSFT(KC_5)),  LALT(LSFT(KC_6)),  LALT(LSFT(KC_7)),  LALT(LSFT(KC_8)),  LALT(LSFT(KC_9)),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  LCTL(KC_RGHT),  LALT(LSFT(KC_E)),  LALT(LSFT(KC_R)),  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS, KC_TRNS, KC_PGDN ,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
+     KC_TRNS,  KC_TRNS,  LALT(LSFT(KC_W)),  LALT(LSFT(KC_E)),  LALT(LSFT(KC_R)),  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS, LALT(LSFT(KC_W)), KC_PGDN ,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
      KC_TRNS,            KC_TRNS,  KC_DEL,  KC_TRNS,  KC_TRNS,  LCTL(KC_LEFT),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,   
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
     
